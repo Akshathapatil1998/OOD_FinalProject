946b3974149ddba0e1c3e20f3f848d31ce8d4148
Sakshi Nayak <nayak.sak@northeastern.edu>
2023-04-13 18:53:06 -0400

    Modified Java and CSS files and added some image folders

---
+++ Final_Project/src/home/Main.java
@@ -0,0 +1,28 @@
+package home;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/Board.fxml"));
+		primaryStage.setTitle("Dollar Dash");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+		// Parent root = FXMLLoader.load(getClass().getResource("fxml/HomeRoot.fxml"));
+		// Scene scene = new Scene(root,400,400);
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		// primaryStage.setScene(scene);
+		// primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
